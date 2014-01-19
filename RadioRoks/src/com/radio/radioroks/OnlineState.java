dcd206fd1597d6f78fc0045c8da5c8e2d20abb7f
Roman <grygorenko.roman@gmail.com>
2014-01-19 16:40:02 +0200

    Recently on stream

--- RadioRoks/src/com/radio/radioroks/OnlineState.java
+++ RadioRoks/src/com/radio/radioroks/OnlineState.java
@@ -2,12 +2,15 @@ package com.radio.radioroks;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.webkit.WebSettings;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
 
 public class OnlineState extends Activity {
 	WebView online;
-	final static String MOBILE_ROKS = "http://m.radioroks.com.ua/";
+	// final static String MOBILE_ROKS = "http://m.radioroks.com.ua/";
+
+	final static String MOBILE_ROKS = "http://www.radioroks.com.ua/digiton/index/inline";
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -15,16 +18,22 @@ public class OnlineState extends Activity {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.online);
 		online = (WebView) findViewById(R.id.webViewOnline);
+		settingParamsForLayout();
+		online.loadUrl(MOBILE_ROKS);
+
+	}
+
+	private void settingParamsForLayout() {
 		online.getSettings().setBuiltInZoomControls(true);
 		online.setFocusableInTouchMode(false);
 		online.setFocusable(false);
-
-		online.loadUrl(MOBILE_ROKS);
 		online.setWebViewClient(new WebViewClient() {
 			public boolean shouldOverrideUrlLoading(WebView view, String url) {
 				return true;
 			}
 		});
 
+		WebSettings settings = online.getSettings();
+		settings.setDefaultTextEncodingName("utf-8");
 	}
 }
